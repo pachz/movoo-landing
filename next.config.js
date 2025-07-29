8bca18bdda3193f4bc8497979cbeb5be4ca25210
Payam Chamani <pchamani@nizek.com>
2025-07-29 19:39:04 +0330

    fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  images: {
+    domains: ["localhost", "127.0.0.1", "sawaai.lon1.digitaloceanspaces.com", "sawaai.cdn.lon1.digitaloceanspaces.com"],
+  },
 };
 
 module.exports = withMDX(nextConfig);
