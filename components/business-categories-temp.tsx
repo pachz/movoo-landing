8a3570bceaf49dd41340377227c7d3827093984d
Payam Chamani <pchamani@nizek.com>
2025-08-05 19:51:14 +0330

    :|

--- components/business-categories-temp.tsx
+++ components/business-categories-temp.tsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 import { Transition } from "@headlessui/react";
 import Logo01 from "@/public/images/logo-01.svg";
-import Logo02 from "@/public/images/logo-02.png";
-import Logo03 from "@/public/images/logo-03.png";
-import Logo04 from "@/public/images/logo-04.png";
-import Logo05 from "@/public/images/logo-05.png";
-import Logo06 from "@/public/images/logo-06.png";
-import Logo07 from "@/public/images/logo-07.png";
-import Logo08 from "@/public/images/logo-08.png";
-import Logo09 from "@/public/images/logo-09.png";
+import Logo02 from "@/public/images/logo-021.svg";
+import Logo03 from "@/public/images/logo-03.svg";
+import Logo04 from "@/public/images/logo-04.svg";
+import Logo05 from "@/public/images/logo-05.svg";
+import Logo06 from "@/public/images/logo-06.svg";
+import Logo07 from "@/public/images/logo-07.svg";
+import Logo08 from "@/public/images/logo-08.svg";
+import Logo09 from "@/public/images/logo-09.svg";
 
 export default function BusinessCategories() {
   const tabsRef = useRef<HTMLDivElement>(null);
