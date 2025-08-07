0dca9c498960ae483357f7755601079264beafc3
Payam Chamani <pchamani@nizek.com>
2025-08-07 14:14:04 +0330

    posthog integ

--- next.config.js
+++ next.config.js
@@ -6,8 +6,32 @@ const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
   images: {
-    domains: ["localhost", "127.0.0.1", "sawaai.lon1.digitaloceanspaces.com", "sawaai.cdn.lon1.digitaloceanspaces.com"],
+    domains: [
+      "localhost",
+      "127.0.0.1",
+      "sawaai.lon1.digitaloceanspaces.com",
+      "sawaai.cdn.lon1.digitaloceanspaces.com",
+    ],
   },
+  // Rewrites for PostHog
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/flags",
+        destination: "https://us.i.posthog.com/flags",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 module.exports = withMDX(nextConfig);
